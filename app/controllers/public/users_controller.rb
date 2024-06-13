488ae444292c83d4055bf04554025bdaed1b35e2
EC2 Default User <166229295+marukome21@users.noreply.github.com>
2024-06-13 03:12:15 +0000

    [Add]マイページ、編集機能

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,2 +1,29 @@
 class Public::UsersController < ApplicationController
+
+  def show
+    @user = current_user
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to users_my_page_path
+  end
+
+  def unsubscribe
+
+  end
+
+  def withdraw
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:user_name, :profile, :user_image, :email, :encrypted_password)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :posts
+  has_one_attached :user_image
+
+  def get_user_image(width, height)
+    unless user_image.attached?
+      file_path = Rails.root.join('app/assets/images/images.jpg')
+      user_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    user_image.variant(resize_to_limit: [width, height]).processed
+  end
+
 end
