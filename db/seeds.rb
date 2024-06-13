0d52726011cd0e3cd2c3d9d7489e04b45faddbf2
EC2 Default User <166229295+marukome21@users.noreply.github.com>
2024-06-13 08:52:28 +0000

    [Add] seed機能（ユーザー）

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Admin.create!(
+   email: 'admin@admin',
+   password: 'testtest'
+)
+
+
+User.create!(
+  user_name: "頑張るクイナ",
+  profile: "毎日がんばるぞ！",
+  email: "yanbaru@gmail.com",
+  password: "password"
+)
+
+User.create!(
+  user_name: "マッスル君",
+  profile: "今日も筋トレだ！",
+  email: "mukimuki@gmail.com",
+  password: "password"
+)
