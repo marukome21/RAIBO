d1fb4aa7ba559cb3e53ed32a38c18c252491e38a
EC2 Default User <166229295+marukome21@users.noreply.github.com>
2024-06-20 14:40:19 +0000

    deploy setting

--- config/puma.rb
+++ config/puma.rb
@@ -43,7 +43,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 plugin :tmp_restart
 
 
-bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+bind "unix:///home/ec2-user/RAIBO/tmp/sockets/puma.sock"
 rails_root = Dir.pwd
 # 本番環境のみデーモン起動
 if Rails.env.production?
