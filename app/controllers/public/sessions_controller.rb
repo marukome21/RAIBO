3f88d3377f91cb49eb4e046e77ff91a3c6df1a17
EC2 Default User <166229295+marukome21@users.noreply.github.com>
2024-06-27 20:37:01 +0000

    [Fix]ゲストユーザーのログインエラー解消

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -6,7 +6,7 @@ class Public::SessionsController < Devise::SessionsController
   def guest_sign_in
     user = User.guest
     sign_in user
-    redirect_to posts_path　　　#, notice: 'guestuserでログインしました。'#フラッシュメッセージ
+    redirect_to posts_path    #, notice: 'guestuserでログインしました。'#フラッシュメッセージ
   end
 
   def destroy
